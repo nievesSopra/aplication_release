bf8917464b1299c572e3049202f1d92325483fdb
nievesSopra <mnievescaracena@gmail.com>
2016-11-09 14:09:37 +0100

    crado json con el contenido de la release.
    modificado el script execute-test.sh para que cree una release en el repo "sauce-project". Pero se trata de una release con info estática.

--- script/execute-test.sh
+++ script/execute-test.sh
@@ -2,8 +2,8 @@
 
 echo "Executing the execute-test.sh file"
 
-#REGEX_TAG="(.*)-TESTME"
-REGEX_TAG="(.*)-t(.*)"
+REGEX_TAG="(.*)-TESTME"
+#REGEX_TAG="(.*)-t(.*)"
 
 
 if [[ "${TRAVIS_TAG}" =~ ${REGEX_TAG} ]]; then
@@ -12,43 +12,40 @@ if [[ "${TRAVIS_TAG}" =~ ${REGEX_TAG} ]]; then
   AUXBRANCH=${VERSION}-Branch01;
   echo "${VERSION}-Branch01"
 
-  echo "Running mvn test command"
-  mvn test -Denv.IPTRAVIS=http://$1;
-  
-  # Rename results folder
-  echo "Rename results folder"
-  cd /home/travis/build/nievesSopra/sauce-project/target; 
-  mv surefire-reports ${VERSION};
-
+  # POST /repos/:owner/:repo/releases
+curl -s -k -X POST -H "Content-Type: application/json" https://${TRAVIS_TOKEN}@github.com/nievesSopra/sauce-project.git -d '{"tag_name": "1234", "target_commitish": "master", "name": "1234", "body": "Release of version 1234", "draft": false, "prerelease": false}'
+ 
   # Configurating git (user.mail and user.name)
-  echo "Configurate git"
-  git config --global user.email "you@example.com";
-  git config --global user.name "travis-arq-test";
+#  echo "Configurate git"
+#  git config --global user.email "you@example.com";
+#  git config --global user.name "travis-arq-test";
+
+
+
 
   # Create the new branch (branch01) and add the folder
-  echo "Creating a new branch"
-  echo "Initialice the repository"
-  git init;
-  echo "Add a remote repository"
-  git remote add test-output https://${TRAVIS_TOKEN}@github.com/nievesSopra/hello-world.git;
-  echo "Creat checkout of the branch"
-  git checkout -b ${VERSION}-Branch01;
-  echo "Adding test folder to the branch"
-  git add -f ${VERSION};
-  echo "Commit the changes"
-  git commit -m "add results folder";
-  echo "Saving changes in the github respository"
-  git push -u test-output ${VERSION}-Branch01;
+#  echo "Creating a new branch"
+#  echo "Initialice the repository"
+#  git init;
+#  echo "Add a remote repository"
+#  git remote add test-output https://${TRAVIS_TOKEN}@github.com/nievesSopra/sauce-project.git;
+#  echo "Creat checkout of the branch"
+#  git checkout -b ${VERSION}-Branch01;
+#  echo "Adding test folder to the branch"
+#  git add -f ${VERSION};
+#  echo "Commit the changes"
+#  git commit -m "add results folder";
+#  echo "Saving changes in the github respository"
+#  git push -u test-output ${VERSION}-Branch01;
 
-  echo "Merge between the bratch and master"
+#  echo "Merge between the bratch and master"
   #Merge branch01 into master branch:
-  git fetch;
-  git checkout -f master;
-  #git merge --allow-unrelated-histories branch01;
-  git merge ${VERSION}-Branch01 -m "Merged branches";
-  git push -u test-output master;
+#  git fetch;
+#  git checkout -f master;
+#  git merge ${VERSION}-Branch01 -m "Merged branches";
+#  git push -u test-output master;
 
   #Delete the branch we created before (branch01):
-  git push test-output --delete ${VERSION}-Branch01
+#  git push test-output --delete ${VERSION}-Branch01
 
 fi
