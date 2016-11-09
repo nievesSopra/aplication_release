097f2996aa25fd58d4102dfca8e65dffee87707a
nievesSopra <mnievescaracena@gmail.com>
2016-11-09 21:05:47 +0100

    Update execute-test.sh

--- script/execute-test.sh
+++ script/execute-test.sh
@@ -16,6 +16,8 @@ REGEX_TAG="(.*)-TESTME"
 #curl -s -k -X POST -H "Content-Type: application/json" https://${TRAVIS_TOKEN}@github.com/nievesSopra/sauce-project.git -d '{"tag_name": "1234", "target_commitish": "master", "name": "1234", "body": "Release of version 1234", "draft": false, "prerelease": false}'
 #curl --data '{"tag_name": "v1.0.0","target_commitish": "master","name": "v1.0.0","body": "Release of version 1.0.0","draft": false,"prerelease": false}' https://api.github.com/repos/:owner/:repository/releases?access_token=:access_token
 
+#curl -u travis-arq-testing:${TRAVIS_TOKEN} --request POST --data '{"tag_name": "v1.0.2-curl","target_commitish": "master","name": "v1.0.2-curl","body": "Release of version 1.0.2","draft": false,"prerelease": false}' https://api.github.com/repos/nievesSopra/sauce-project/releases
+
 API_JSON=$(printf '{"tag_name": “v%s-TESTME”,”target_commitish": "master","name": “v%s-TESTME”,”body": "Release of version %s-TESTME”,”draft": false,"prerelease": false}' $VERSION $VERSION $VERSION)
 curl -u travis-arq-testing:${TRAVIS_TOKEN} --request POST --data “$API_JSON” https://api.github.com/repos/nievesSopra/sauce-project/releases
 
