70cdf288f9be4dc025ae26bccbcbc98a54644733
nievesSopra <mnievescaracena@gmail.com>
2016-11-09 21:47:11 +0100

    Update execute-test.sh

--- script/execute-test.sh
+++ script/execute-test.sh
@@ -8,11 +8,11 @@ REGEX_TAG="(.*)-TESTME"
 
 #if [[ "${TRAVIS_TAG}" =~ ${REGEX_TAG} ]]; then
   # Save version
-  VERSION=${BASH_REMATCH[1]}-Results;
-  AUXBRANCH=${VERSION}-Branch01;
-  echo "${VERSION}-Branch01"
+#  VERSION=${BASH_REMATCH[1]};
 
-  echo “TRAVIS TAG: ${TRAVIS_TAG}”
+  echo "${VERSION}"
+
+  echo “TRAVIS TAG: ${TRAVIS_TAG} TRAVIS_REPO_SLUG: ${TRAVIS_REPO_SLUG}”
 
   # POST /repos/:owner/:repo/releases
 #curl -s -k -X POST -H "Content-Type: application/json" https://${TRAVIS_TOKEN}@github.com/nievesSopra/sauce-project.git -d '{"tag_name": "1234", "target_commitish": "master", "name": "1234", "body": "Release of version 1234", "draft": false, "prerelease": false}'
@@ -20,8 +20,9 @@ REGEX_TAG="(.*)-TESTME"
 
 #curl -u travis-arq-testing:${TRAVIS_TOKEN} --request POST --data '{"tag_name": "v1.0.2-curl","target_commitish": "master","name": "v1.0.2-curl","body": "Release of version 1.0.2","draft": false,"prerelease": false}' https://api.github.com/repos/nievesSopra/sauce-project/releases
 
-API_JSON=$(printf '{"tag_name": “v%s-TESTME”,”target_commitish": "master","name": “v%s-TESTME”,”body": "Release of version %s-TESTME”,”draft": false,"prerelease": false}' $VERSION $VERSION $VERSION)
-curl -u travis-arq-testing:${TRAVIS_TOKEN} --request POST --data “$API_JSON” https://api.github.com/repos/nievesSopra/sauce-project/releases
+# Create release in sauce-project repository
+#API_JSON=$(printf '{"tag_name": “v%s-TESTME”,”target_commitish": "master","name": “v%s-TESTME”,”body": "Release of version %s-TESTME”,”draft": false,"prerelease": false}' $VERSION $VERSION $VERSION)
+#curl -u travis-arq-testing:${TRAVIS_TOKEN} --request POST --data “$API_JSON” https://api.github.com/repos/nievesSopra/sauce-project/releases
 
 #API_JSON=$(printf '{"tag_name": "v%s","target_commitish": "master","name": "v%s","body": "Release of version %s","draft": false,"prerelease": false}' $VERSION $VERSION $VERSION)
 #curl --data "$API_JSON" https://api.github.com/repos/:owner/:repository/releases?access_token=:access_token
